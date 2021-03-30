fec46c57f96ca2d91b472d3961fb92564cb3cb3b
ntvu111 <ntvu111@gmail.com>
2021-03-30 19:32:55 +0700

    update slider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -18,6 +18,29 @@ $(document).ready(function(){
 
 
 
+  $('.grid-item').on('click',function(){
+    var current = $(this).index();
+    $('.gallery-slider').addClass('gallery-slider-active');
+    $('.grid-layout').children('.grid-item ').each(function() {
+      $('.gallery-slider-content .swiper-wrapper').append(
+          '<div class="swiper-slide gallery-slider__item">'+
+            '<img class="swiper-lazy" data-src="' + $(this).find('img').attr('src') + '">'+
+            '<div class="swiper-lazy-preloader swiper-lazy-preloader-white"></div>'+
+          '</div>'
+        );
+    });
+    var portfolio5 = new Swiper('.gallery-slider-content', {
+      slidesPerView: 1,
+      initialSlide:current,
+      lazy:true
+    });
+    $('.gallery-slider__close').on('click',function(){
+      $('.gallery-slider').removeClass('gallery-slider-active');
+      $('.gallery-slider-content .swiper-wrapper .swiper-slide').remove();
+      portfolio5.destroy();
+    });
+  });
+  
 
 });
  //=========================== blog mansory===================================
